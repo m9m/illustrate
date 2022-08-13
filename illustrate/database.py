3e36eacb2b720ed9377216f2d09ed8166c2a9f68
m9m <>
2022-08-13 17:05:22 -0500

    updated setup_tools.py

--- illustrate/database.py
+++ illustrate/database.py
@@ -1,6 +1,4 @@
 import sqlite3
-from sqlite3 import Error
-
 
 class DatabaseUtilities :
     def __init__(self):
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='illustrate',
+    version='1.0',
+    author='m9m',
+    packages=find_packages()
+)
